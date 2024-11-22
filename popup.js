11a3fa7cfa319c06f337e1598b0b35717e29d50b
konishi333 <jobhuntingkonishi3@gmail.com>
2024-11-22 16:21:37 +0900

    クリップ機能実装
    フィルターとクリップは非表示/表示となるようポップアップ画面を修正

--- popup.js
+++ popup.js
@@ -11,15 +11,30 @@ window.addEventListener('load', function() {
         document.getElementById("invert").value = response.invert;
         document.getElementById("blurred").value = response.blurred;
         document.getElementById("opacity").value = response.opacity;
+        
         leftRightReverse = response.leftRightReverse;
         upDownReverse = response.upDownReverse;
         hideControls = response.hideControls;
-
         if (hideControls) {
           document.getElementById("hideControls").textContent = "コントロール表示";
         } else {
           document.getElementById("hideControls").textContent = "コントロール非表示";
         }
+
+        let clipStartTime = response.clipStartTime;
+        let startTime = seconds2time(clipStartTime)
+        document.getElementById("startTimeHh").value = startTime.hhTime;
+        document.getElementById("startTimeMm").value = startTime.mmTime;
+        document.getElementById("startTimeSs").value = startTime.ssTime;
+        document.getElementById("startTimeMs").value = startTime.msTime;
+
+        let clipEndTime = response.clipEndTime;
+        let endTime = seconds2time(clipEndTime)
+        document.getElementById("endTimeHh").value = endTime.hhTime;
+        document.getElementById("endTimeMm").value = endTime.mmTime;
+        document.getElementById("endTimeSs").value = endTime.ssTime;
+        document.getElementById("endTimeMs").value = endTime.msTime;
+
       }
     });
   });
@@ -29,6 +44,14 @@ document.getElementById("reset").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { command: "reset" }, (response) => {
       if (response?.apply) {
+        document.getElementById("startTimeHh").value = 0;
+        document.getElementById("startTimeMm").value = 0;
+        document.getElementById("startTimeSs").value = 0;
+        document.getElementById("startTimeMs").value = 0;
+        document.getElementById("endTimeHh").value = 0;
+        document.getElementById("endTimeMm").value = 1;
+        document.getElementById("endTimeSs").value = 0;
+        document.getElementById("endTimeMs").value = 0;
         document.getElementById("brightness").value = 1
         document.getElementById("contrast").value = 1
         document.getElementById("saturate").value = 1
@@ -44,6 +67,79 @@ document.getElementById("reset").addEventListener("click", () => {
     });
   });
 });
+
+let isOpenClip = false;
+document.getElementById("clip").addEventListener("click", () =>{
+  const filters = document.getElementById("clipTimes");
+  if (isOpenClip) {
+    document.getElementById("clip").innerText = "クリップ表示"
+    filters.style.display = "none";
+  } else {
+    document.getElementById("clip").innerText = "クリップ非表示"
+    filters.style.display = "block";
+  }
+  isOpenClip = !isOpenClip
+});
+
+document.getElementById("clipApply").addEventListener("click", () => {
+  sendClipQuery()
+});
+
+document.getElementById("clipReset").addEventListener("click", () => {
+  let hhStart = parseInt(document.getElementById("startTimeHh").value, 10);
+  let mmStart = parseInt(document.getElementById("startTimeMm").value, 10);
+  let ssStart = parseInt(document.getElementById("startTimeSs").value, 10);
+  let msStart = parseInt(document.getElementById("startTimeMs").value, 10);
+
+  let hhEnd = parseInt(document.getElementById("endTimeHh").value, 10);
+  let mmEnd = parseInt(document.getElementById("endTimeMm").value, 10);
+  let ssEnd = parseInt(document.getElementById("endTimeSs").value, 10);
+  let msEnd = parseInt(document.getElementById("endTimeMs").value, 10);
+
+  let startTime = time2seconds(hhStart,mmStart,ssStart,msStart)
+  let endTime = time2seconds(hhEnd,mmEnd,ssEnd,msEnd)
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { command: "clipEnd", startTime: startTime, endTime: endTime});
+  });
+});
+
+let isOpenFilters = false
+document.getElementById("filter").addEventListener("click", () =>{
+  const filters = document.getElementById("filterSliders");
+  if (isOpenFilters) {
+    document.getElementById("filter").innerText = "フィルター表示"
+    filters.style.display = "none";
+  } else {
+    document.getElementById("filter").innerText = "フィルター非表示"
+    filters.style.display = "block";
+  }
+  isOpenFilters = !isOpenFilters
+});
+
+function sendClipQuery() {
+  let hhStart = parseInt(document.getElementById("startTimeHh").value, 10);
+  let mmStart = parseInt(document.getElementById("startTimeMm").value, 10);
+  let ssStart = parseInt(document.getElementById("startTimeSs").value, 10);
+  let msStart = parseInt(document.getElementById("startTimeMs").value, 10);
+  
+  let hhEnd = parseInt(document.getElementById("endTimeHh").value, 10);
+  let mmEnd = parseInt(document.getElementById("endTimeMm").value, 10);
+  let ssEnd = parseInt(document.getElementById("endTimeSs").value, 10);
+  let msEnd = parseInt(document.getElementById("endTimeMs").value, 10);
+  
+  let startTime = time2seconds(hhStart,mmStart,ssStart,msStart)
+  let endTime = time2seconds(hhEnd,mmEnd,ssEnd,msEnd)
+
+  if ( startTime >= endTime) {
+    alert('開始時刻が終了時刻を上まってしまっています。開始時刻と終了時刻を設定し直してください。')
+    return;
+  }
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { command: "clip", startTime: startTime, endTime: endTime });
+  });
+}
 
 document.getElementById("brightness").addEventListener("input", () => {
   const brightness = document.getElementById("brightness").value;
@@ -163,3 +259,33 @@ document.getElementById("camera").addEventListener("click", () => {
   });
 });
 
+function time2seconds(hhTime, mmTime, ssTime, msTime) {
+  let seconds = 0;
+  seconds += hhTime * 3600; // 時間を秒に変換
+  seconds += mmTime * 60;   // 分を秒に変換
+  seconds += ssTime;        // 秒
+  seconds += msTime / 100;  // ミリ秒
+
+  return seconds
+}
+
+function seconds2time(totalSeconds) {
+  // 時間を計算
+  let hhTime = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+
+  // 分を計算
+  let mmTime = Math.floor(totalSeconds / 60);
+  totalSeconds %= 60;
+
+  // 秒とミリ秒を計算
+  let ssTime = Math.floor(totalSeconds);
+  let msTime = Math.round((totalSeconds - ssTime) * 100);
+  
+  return {
+    hhTime: hhTime,
+    mmTime: mmTime,
+    ssTime: ssTime,
+    msTime: msTime
+  };
+}
